9460582d48cfc6cf8c15e3b318b80b32ff0352dc
jorisguffens <joris.guffens@outlook.com>
2023-02-20 20:45:27 +0000

    Deploying to javadocs from @ GufliMC/Config@4d89dfc0388c2e18c0559a4c6ad8509174398675 🚀

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.guflimc.config.common"},{"l":"com.guflimc.config.toml"}];updateSearchResults();
---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.guflimc.config.common","l":"Config"},{"p":"com.guflimc.config.common","l":"ConfigComment"},{"p":"com.guflimc.config.toml","l":"TomlConfig"}];updateSearchResults();
